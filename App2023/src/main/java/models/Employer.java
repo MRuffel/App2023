9a896efaea0fc04e80da41e0b70a0fd85110b149
MRuffel <47765411+MRuffel@users.noreply.github.com>
2023-01-16 12:24:00 -0500

    Ajouter des variables

--- App2023/src/main/java/models/Employer.java
+++ App2023/src/main/java/models/Employer.java
@@ -1,4 +1,22 @@
 package models;
 
 public class Employer {
+
+
+    String name;
+
+    public Employer(String name) {
+        this.name = name;
+    }
+
+    public Employer() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employer{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
