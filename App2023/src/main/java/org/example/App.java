361427940e4cfca8b300d4fcc6f6a51f4ffc2f58
MRuffel <47765411+MRuffel@users.noreply.github.com>
2023-01-16 12:47:12 -0500

    Main change

--- App2023/src/main/java/org/example/App.java
+++ App2023/src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import models.Employer;
+
 /**
  * Hello world!
  *
  */
+
+
+
+
 public class App 
 {
+
+    static Employer emp1 = new Employer("John");
     public static void main( String[] args )
     {
         System.out.println( "Java 2023!" );
+        System.out.println( emp1.toString() );
     }
+
+
 }
